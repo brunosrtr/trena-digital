cc231c56a686bdb71a98753a76555c3351256716
Bruno <bruno.o.sartori.2@gmail.com>
2025-12-04 15:02:40 -0300

    Refactor ultrasonic sensor code and LCD output

--- trenadigital.ino
+++ trenadigital.ino
@@ -1,10 +1,8 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-// Endereço do LCD 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-// Pinos do sensor ultrassônico
 int trigPin = 9;
 int echoPin = 10;
 
@@ -25,26 +23,22 @@ void setup() {
 }
 
 void loop() {
-  // Envia pulso ultrassônico
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
 
-  // Recebe o pulso de volta e calcula duração
   duracao = pulseIn(echoPin, HIGH);
 
-  // Calcula a distância em cm (velocidade do som / 2)
   distancia = duracao * 0.034 / 2;
 
-  // Mostra no LCD
   lcd.setCursor(0, 0);
   lcd.print("Distancia:");
 
   lcd.setCursor(0, 1);
   lcd.print(distancia);
-  lcd.print(" cm   "); // Os espaços apagam restos anteriores
+  lcd.print(" cm   "); 
 
-  delay(500); // Atualiza a cada meio segundo
+  delay(500);
 }
